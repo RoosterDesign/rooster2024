eb47f273f3f7ff826ea25463ff87ac2947f0d205
Neil <neil@roosterdesign.co.uk>
2024-08-22 17:37:31 +0100

    more about dev

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,11 +1,13 @@
 import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
 import AboutMasthead from "@/components/AboutMasthead/AboutMasthead";
+import SkillsGraph from "@/components/SkillsGraph/SkillsGraph";
 
 export default function About() {
     return (
         <>
             <Breadcrumbs />
             <AboutMasthead />
+            <SkillsGraph />
         </>
     )
 }
---
+++ src/components/SkillsGraph/SkillsGraph.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import Container from '@/components/Container/Container';
+import Button from '@/components/Button/Button';
+
+import styles from './SkillsGraph.module.scss';
+
+export default function SkillsGraph() {
+
+    const skillLogos = ['html5', 'css3', 'javascript', 'react', 'nextjs', 'tailwind', 'wordpress', 'figma'];
+
+    return (
+        <section className={`${styles.skillsGraph} block`}>
+            <Container>
+
+                <div className={styles.skillsWrap}>
+                    {skillLogos.map((logo, index) => (
+                        <div className={styles.skill} key={index}>
+                            <Image src={`/skill-logos/${logo}.svg`} alt="" height={0} width={0} />
+                        </div>
+                    ))}
+                    <div className={styles.skillPhoto}>
+                        <Image src={`/neil-headshot.jpg`} alt="" height={300} width={300} />
+                    </div>
+                </div>
+
+                <div className={styles.intro}>
+                    <h3>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tristique arcu quis nibh rhoncus, vel faucibus lorem semper. Ut consequat mi id diam blandit.`}</h3>
+                    <p>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tristique arcu quis nibh rhoncus, vel faucibus lorem semper. Ut consequat mi id diam blandit, ac consequat.`}</p>
+                    <Button url={``}  label={`My services`} />
+                </div>
+
+            </Container>
+        </section>
+    )
+}

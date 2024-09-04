d8eadfa9536e5c66b4d646ee87536d0fc2c8f6cd
Neil <neil@roosterdesign.co.uk>
2024-09-04 17:43:07 +0100

    fix build errors

--- src/components/LinkCard/LinkCardType.ts
+++ src/components/LinkCard/LinkCardType.ts
@@ -2,7 +2,7 @@ import { StaticImageData } from 'next/image';
 
 export interface LinkCardType {
     image?: StaticImageData;
-    icon: React.ReactNode;
+    icon?: React.ReactNode;
     title: string;
     body: string;
     link: string;

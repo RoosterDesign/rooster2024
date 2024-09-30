8272307b52dad6877cdeea0992f0e672cf7b5241
Neil <neil@roosterdesign.co.uk>
2024-09-30 11:38:06 +0100

    vercel fix attempt

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,15 +1,18 @@
-import { StoryblokStory } from "@storyblok/react/rsc";
-import { fetchData } from '@/lib/storyblok';
+// import { StoryblokStory } from "@storyblok/react/rsc";
+// import { fetchData } from '@/lib/storyblok';
 
-export default async function About() {
-    const sbOptions = {
-        slug: "about",
-        resolve_relations: ["testimonials_list.items"]
-    }
-    const { data } = await fetchData(sbOptions);
-    const bridgeOptions = { resolveRelations: ["testimonials_list.items"] };
+const About: React.FC = async () => {
+    // const sbOptions = {
+    //     slug: "about",
+    //     resolve_relations: ["testimonials_list.items"]
+    // }
+    // const { data } = await fetchData(sbOptions);
+    // const bridgeOptions = { resolveRelations: ["testimonials_list.items"] };
 
     return (
-        <StoryblokStory story={data.story} bridgeOptions={bridgeOptions} />
+        <h1>About</h1>
+        // <StoryblokStory story={data.story} bridgeOptions={bridgeOptions} />
     )
 }
+
+export default About;
